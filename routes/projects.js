16ff9ec6b626320f200c2e4ce96a27c16e6631da
Technopreneur1 <askemailer@gmail.com>
2014-07-30 15:38:28 +0100

    header

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,10 @@ var router = express.Router(); //need the router method in express
 var db = require('../models/projects.js');
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send(db); //res= response. Not building a template, sending some data back
+  res.send(db);
+  response.writeHead(200, {"Content-Type": "text/json", 'Access-Control-Allow-Origin': '*'});
+response.end(data);
+//res= response. Not building a template, sending some data back
 }); //controller: given some route, what you should do in repsonse to it
 
 
