bf0b627ba967535e276e3f4f0c51b6e6ae616f27
Technopreneur1 <askemailer@gmail.com>
2014-07-31 15:42:16 +0100

    asd

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ var projects = require('./routes/projects');
 
 var app = express(); 
 
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
